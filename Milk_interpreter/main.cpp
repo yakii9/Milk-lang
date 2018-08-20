9e9a7b38029cbd10bb59d1bb67addd7b3b47c959
段诗闻 <yakii9@vip.qq.com>
2018-08-20 19:59:32 +0800

    Initial Commit

---
+++ Milk_interpreter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Milk_interpreter
+//
+//  Created by 段诗闻 on 2018/8/20.
+//  Copyright © 2018年 段诗闻. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
